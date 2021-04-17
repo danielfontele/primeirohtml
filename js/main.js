bb8ba2e38fbadb4c8ec91aa31a88f693445da99e
Daniel Fontele <danielfontele@hotmail.com>
2021-04-17 15:22:24 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    //    autoplay: true,
+    //    autoplaySpeed: 4000
+});
